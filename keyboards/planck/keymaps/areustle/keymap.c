25079ed2286123b377485307b5a761da5cc6ee9a
Alex Reustle <areustledev@gmail.com>
2019-10-30 15:46:52 -0400

    New updates to the areustle keymap

--- keyboards/planck/keymaps/areustle/keymap.c
+++ keyboards/planck/keymaps/areustle/keymap.c
@@ -55,12 +55,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,      KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,          KC_H,      KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,          KC_N,      KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    A(KC_TAB), KC_LCTL, KC_LALT, KC_LGUI, LOWER, CTL_T(KC_SPC), KC_SFTENT, RAISE, KC_BTN1, KC_BTN2, KC_LCTL, KC_LSFT
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,      KC_Y,      KC_U,  KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,      KC_H,      KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,      KC_N,      KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+    G(KC_TAB), KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_BSPC), SFT_T(KC_SPC), CTL_T(KC_SPC), LT(RAISE, KC_ENT), KC_RGUI, KC_RALT, KC_LCTL, KC_LSFT
 ),
-
+//  
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LCBR, KC_RCBR, KC_PLUS, KC_MINS, _______, _______, _______,
     _______, KC_TILD, KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_EQL,  KC_ASTR, KC_UNDS, KC_0,    KC_SCLN,
     _______, _______, _______, KC_BSLS, KC_LBRC, KC_LABK, KC_RABK, KC_RBRC, KC_SLSH, KC_PIPE, KC_UP,  _______,
-    _______, _______, _______, _______, _______, KC_BSPC, KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT
+    _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_DEL,  _______, KC_RSFT, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 /* Raise
