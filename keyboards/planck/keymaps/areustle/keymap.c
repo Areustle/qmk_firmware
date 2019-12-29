5407dfb3c830dcca044e9619490fd6b9f7de1d60
Alex Reustle <areustledev@gmail.com>
2019-12-29 13:36:42 -0500

    New map for raise & lower

--- keyboards/planck/keymaps/areustle/keymap.c
+++ keyboards/planck/keymaps/areustle/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,      KC_N,      KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
     G(KC_TAB), KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_BSPC), CTL_T(KC_ESC), SFT_T(KC_ENT), LT(RAISE, KC_SPC), KC_RGUI, KC_RALT, KC_LCTL, KC_LSFT
 ),
-//  
+//
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
@@ -99,27 +99,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  _   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | ATa  |   1  |   2  |   3  |   4  |   5  |   +  |   _  |   =  |   {  |   }  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |   6  |   7  |   8  |   9  |   0  |   [  |   ]  |   <  |   >  |   ?  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      | ctrl |  alt |  gu  |      | Bspc | Del  |      | Sft  | Vol- | Vol+ | Mute |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LCBR, KC_RCBR, KC_EQL, KC_PLUS, _______, _______, _______,
-    _______, KC_TILD, KC_PERC, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, KC_ASTR, KC_SCLN, KC_QUOT,
-    _______, _______, _______, KC_BSLS, KC_LBRC, KC_LABK, KC_RABK, KC_RBRC, KC_SLSH, KC_PIPE, KC_UP,  _______,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_DEL,  _______, KC_RSFT, KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_TILD,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
+    A(KC_TAB), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PLUS, KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_DQUO,
+    _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_QUES, KC_PIPE,
+    _______,   KC_LCTL, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_DEL,  _______, KC_RSFT, KC_VOLD, KC_VOLU, KC_MUTE
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -127,10 +127,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    _______, KC_GRV,  KC_1,    KC_2, KC_3,    KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,  _______, _______,
-    _______, KC_TILD, KC_4,    KC_5, KC_6,    KC_0,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-    _______, _______, KC_7,    KC_8, KC_9,    KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_BTN2, _______,
-    _______, _______, _______, KC_0, _______, KC_BSPC, KC_DEL,  _______, _______, _______,  _______, _______
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    _______,
+    _______, KC_TILD, KC_CIRC, KC_DLR,  KC_MINS, KC_0,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+    _______, KC_BRIU, KC_BRID, KC_PSTE, KC_COPY, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_BTN2, _______,
+    _______, _______, _______, _______, _______, KC_BSPC, KC_DEL,  _______, _______, _______,  _______, _______
 ),
 
 /* Plover layer (http://opensteno.org)
